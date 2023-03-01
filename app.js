0cca03b034adbb148e43633a8e8c658a608e3f87
Maziar <Maziar.mod@gmail.com>
2023-03-01 14:23:21 -0500

    add drivers_controller_test and add create method for adding a driver in driver_controller.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/muber');
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
